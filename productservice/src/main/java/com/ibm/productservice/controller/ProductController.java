27ca30f3db8eea7f557cb738aed0ca8ccc50487f
w2cluster225 <w2cluster225@w2cluster246.iiht.tech>
2020-11-02 16:41:34 +0530

    added swagger

--- productservice/src/main/java/com/ibm/productservice/controller/ProductController.java
+++ productservice/src/main/java/com/ibm/productservice/controller/ProductController.java
@@ -1,6 +1,10 @@
 package com.ibm.productservice.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +17,8 @@ import com.ibm.productservice.dto.ProductDTO;
 import com.ibm.productservice.entity.Product;
 import com.ibm.productservice.service.ProductService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RequestMapping("product")
 @RestController
 public class ProductController {
@@ -22,15 +28,29 @@ public class ProductController {
 	ProductService productService;
 	
 	@PostMapping("/")
-	public Product products(@RequestBody ProductDTO dto) {
+	@ApiOperation("This will create product entity")
+	public ResponseEntity<Product> products(@RequestBody ProductDTO dto) {
 		
-		return productService.createProduct(dto);
+		
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(dto));
 	}
 	
+	@ApiOperation("This return the product for given id")
 	@GetMapping("/") //localhost:8080/product?id=1
-	public Product products(@RequestParam(value="id")Long id) {
+	public ResponseEntity<Product> products(@RequestParam(value="id")Long id) {
 		
-		return productService.getProduct(id).get();
+		Optional<Product> product= productService.getProduct(id);
+		
+		if(product.isPresent()) {
+			
+			return ResponseEntity.status(HttpStatus.OK).body(product.get());
+		}else {
+			
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
+		
+		
 		
 	}
 	
